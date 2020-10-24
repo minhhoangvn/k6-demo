a99f5a6726543b8808fd45dd6e77d2b403440ced
minhhoang <minhhoang@kms-technology.com>
2020-10-25 01:26:27 +0700

    Update Techshow demo

--- scenario-tests/search-and-buy-items.js
+++ scenario-tests/search-and-buy-items.js
@@ -15,8 +15,8 @@ var thresholdConfig = {
   'http_reqs{scenario_name:unregister_user}': ['count >= 1'],
   'checks{scenario_name:unregister_user}': ['rate > 0.9'],
   'check_failure_rate{scenario_name:unregister_user}': ['rate < 0.1'],
-  'errors{scenario_name:unregister_user}': ['count < 5'],
-  'waiting_timing{scenario_name:unregister_user}': [
+  'check_errors{scenario_name:unregister_user}': ['count < 5'],
+  'check_waiting_timing{scenario_name:unregister_user}': [
     'p(90) < 600',
     'p(70) < 500',
     'avg < 450',
@@ -28,8 +28,8 @@ var thresholdConfig = {
   'http_reqs{scenario_name:register_user}': ['count >= 1'],
   'checks{scenario_name:register_user}': ['rate > 0.9'],
   'check_failure_rate{scenario_name:register_user}': ['rate < 0.1'],
-  'errors{scenario_name:register_user}': ['count < 5'],
-  'waiting_timing{scenario_name:register_user}': [
+  'check_errors{scenario_name:register_user}': ['count < 5'],
+  'check_waiting_timing{scenario_name:register_user}': [
     'p(90) < 600',
     'p(70) < 500',
     'avg < 450',
@@ -41,8 +41,8 @@ var thresholdConfig = {
   'http_reqs{userAction:searchShirt}': ['count >= 1'],
   'checks{userAction:searchShirt}': ['rate > 0.9'],
   'check_failure_rate{userAction:searchShirt}': ['rate < 0.1'],
-  'errors{userAction:searchShirt}': ['count < 5'],
-  'waiting_timing{userAction:searchShirt}': [
+  'check_errors{userAction:searchShirt}': ['count < 5'],
+  'check_waiting_timing{userAction:searchShirt}': [
     'p(90) < 600',
     'p(70) < 500',
     'avg < 450',
@@ -54,18 +54,23 @@ var thresholdConfig = {
   'http_reqs{userAction:searchWine}': ['count >= 1'],
   'checks{userAction:searchWine}': ['rate > 0.9'],
   'check_failure_rate{userAction:searchWine}': ['rate < 0.1'],
-  'errors{userAction:searchWine}': ['count < 5'],
-  'waiting_timing{userAction:searchWine}': ['p(90) < 600', 'p(70) < 500', 'avg < 450', 'med < 450'],
+  'check_errors{userAction:searchWine}': ['count < 5'],
+  'check_waiting_timing{userAction:searchWine}': [
+    'p(90) < 600',
+    'p(70) < 500',
+    'avg < 450',
+    'med < 450',
+  ],
 };
 
 module.exports.requests = new Counter('http_reqs');
 // Custom rate metric
 var checkFailureRate = new Rate('check_failure_rate');
 // Custom counter metrics
-var countErrors = new Counter('errors');
+var countErrors = new Counter('check_errors');
 
 // Custom trend metrics
-var waitingTimingTrend = new Trend('waiting_timing');
+var waitingTimingTrend = new Trend('check_waiting_timing');
 
 var buyItemWithUnregisterUserTestScenario = {
   exec: 'buyItemWithUnregisterUser',
