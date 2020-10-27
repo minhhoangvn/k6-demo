acf0c6d194f2d5a3d91329bb229234c025b323c6
minhhoang <minhhoang@kms-technology.com>
2020-10-27 17:05:15 +0700

    Update techshow demo

--- scenario-tests/search-and-buy-items.js
+++ scenario-tests/search-and-buy-items.js
@@ -7,8 +7,8 @@ var search = require('../services/search-items/index.js');
 var select = require('../services/select-items/index.js');
 var account = require('../services/create-accounts/index.js');
 
-var ITERATION = 2;
-var VUs = 100;
+var ITERATION = 10;
+var VUs = 10;
 var thresholdConfig = {
   'Content OK': ['rate > 0.95'],
   // scenario 1
